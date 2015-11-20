f5173172f3fdf355f681423af63aa76f17203baf
ross <rosshinkley@gmail.com>
2015-11-19 22:26:12 -0500

    adding crosh palette

---
+++ crosh-palette.js
@@ -0,0 +1,23 @@
+base03 = "#002b36";
+base02 = "#073642";
+base01 = "#586e75";
+base00 = "#657b83";
+base0 = "#839496";
+base1 = "#93a1a1";
+base2 = "#eee8d5";
+base3 = "#fdf6e3";
+yellow = "#FFF700";
+orange = "#cb4b16";
+red = "#FF0000";
+magenta = "#d33682";
+violet = "#6c71c4";
+blue = "#268bd2";
+cyan = "#2aa198";
+green = "#009900";
+
+term_.prefs_.set('color-palette-overrides', [
+    base02, red, green, yellow,
+    blue, magenta, cyan, base2,
+    base03, orange, base01, base00,
+    base0, violet, base1, base3
+]);
